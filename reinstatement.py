5d95e50553ed531123fbd4eac18bc473dabfc420
Revaldy Hazza Daniswara <diohazza123@gmail.com>
2025-07-07 10:59:47 +0700

    Update reinstatement.py

--- reinstatement.py
+++ reinstatement.py
@@ -312,6 +312,7 @@ def ringkasan_data_asli(df_soc_real, ur, layer):
         })
     
     # Buat DataFrame tanpa baris Total
+    summary_data = summary_data.drop(index=8, inplace=True)
     df_summary = pd.DataFrame(summary_data)
     
     return df_summary
